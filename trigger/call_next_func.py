65b82d97eec254baa3ed742dbea72e514d85ed32
Reza <r.malek@keemail.me>
2024-11-27 16:26:14 +0100

    fix trigger func calling riskeval instead of collisiondetector

--- trigger/call_next_func.py
+++ trigger/call_next_func.py
@@ -10,8 +10,8 @@ host = "172.17.0.1"
 logger = logging.getLogger(__name__)
 
 
-def post_risk_eval(data, meta):
-    url = f"http://{host}:8000/riskeval"
+def post_collision_detector(data, meta):
+    url = f"http://{host}:8000/collisiondetector"
     headers = {
         "Content-Type": "application/json",
         "X-tinyFaaS-Async": "true"}  # tinyfaas will return a 202 response
@@ -20,10 +20,10 @@ def post_risk_eval(data, meta):
         "data": data,
         "meta": meta
     }
-    logger.debug(f'[trigger fn] calling riskeval function on {url} with payload: {payload}')
+    logger.debug(f'[trigger fn] calling collisiondetector function on {url} with payload: {payload}')
     response = requests.post(url, headers=headers, data=JSONEncoder().encode(payload))
     if response.status_code != 202:  # async call
-        logger.error(f'[trigger fn] Error calling riskeval function ({response.status_code}): {response.text}')
+        logger.error(f'[trigger fn] Error calling collisiondetector function ({response.status_code}): {response.text}')
     else:
-        logger.info(f'[trigger fn] ({response.status_code}) Response from riskeval function: {response.text}')
+        logger.info(f'[trigger fn] ({response.status_code}) Response from collisiondetector function: {response.text}')
     return response
